1ef4afe0a075956c1d8bfa6d371fd8fba9d4fb5b
Evans <156232942+3vqns@users.noreply.github.com>
2025-03-28 17:53:08 -0400

    Delete front-end directory

--- front-end/src/CatalogTitle.jsx
+++
@@ -1,9 +0,0 @@
-import './rubric.css'
-
-export default function CatalogTitle({name, description}){
-    return (
-        <div className="category">
-            {name} : {description}
-        </div>
-    )
-}
