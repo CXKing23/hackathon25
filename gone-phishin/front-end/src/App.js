b1d1aac3c2dd0c2fca7caaab42fdd6b85a7ad4cf
MustafaProgram <mustafaakilli24@gmail.com>
2025-03-29 00:18:37 -0400

    Made logo spin

--- gone-phishin/front-end/src/App.js
+++ gone-phishin/front-end/src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+import Logo from './Logo.png'; // Adjust the path as needed
+import React from 'react';
 import './App.css';
 import EmailScanner from './EmailScanner';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <EmailScanner />
+      
     </div>
   );
 }
